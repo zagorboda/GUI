0709562884db9245504df3cbb99fa073c867dcf0
zagorboda <zagorboda@gmail.com>
2019-11-10 19:08:17 +0300

    Added matrix's shape input. Fixed bugs with functions and displaying widgets

--- Matrix Calculator.py
+++ Matrix Calculator.py
@@ -7,7 +7,7 @@ from tkinter.messagebox import *
 root = Tk()
 root.title("Matrix Calculator")
 
-m, n = 2, 2
+
 c = []
 d = []
 b = [[0 for x in range(2)] for y in range(2)]
@@ -15,9 +15,33 @@ b = [[0 for x in range(2)] for y in range(2)]
 show_empty_input = False
 show_nondigit_input = False
 
+entry_m = Entry(root)
+entry_m.grid(row=0, column=0)
+entry_n = Entry(root)
+entry_n.grid(row=1, column=0)
 
-def calculate():
-    global c,d
+
+def show():
+    global m
+    m = int(entry_m.get())
+    global n
+    n = int(entry_n.get())
+    a = [[0 for x in range(n)] for y in range(m)]
+    for i in range(m):
+        for j in range(n):
+            entry = Entry(root, width=10)
+            entry.grid(row=i, column=j)
+            a[i][j] = entry
+    btn1 = Button(root, text="Button", command=lambda: calculate(m, n, a))
+    btn1.grid(row=4, column=0, columnspan=3)
+
+
+btn_start = Button(root, text="Start", command=lambda: show())
+btn_start.grid(row=3, column=0)
+
+
+def calculate(m, n, a):
+    global c, d
     global show_empty_input, show_nondigit_input
     show_empty_input = False
     show_nondigit_input = False
@@ -41,7 +65,8 @@ def calculate():
     d = np.reshape(d, (2, 2))
     print(d)
     print("%.6f" % np.linalg.det(d))
-    lbl1.configure(text="Matrix determinant = %f" % np.linalg.det(d))
+    lbl1 = Label(root, text="Matrix determinant = %f" % np.linalg.det(d))
+    lbl1.grid(row=5, column=0, columnspan=3)
 
 # entry1 = Entry(root)
 # entry1.grid(row=0, column=0)
@@ -54,19 +79,19 @@ def calculate():
 # lbl1.grid(row=0, column=2)
 
 
-a = [[0 for x in range(2)] for y in range(2)]
-
-for i in range(m):
-    for j in range(n):
-        entry = Entry(root, width=10)
-        entry.grid(row=i, column=j)
-        a[i][j] = entry
+# a = [[0 for x in range(2)] for y in range(2)]
+#
+# for i in range(m):
+#     for j in range(n):
+#         entry = Entry(root, width=10)
+#         entry.grid(row=i, column=j)
+#         a[i][j] = entry
 
 # command=lambda: calculate()
-btn1 = Button(root, text="Button", command=lambda: calculate())
-btn1.grid(row=4, column=0, columnspan=3)
+# btn1 = Button(root, text="Button", command=lambda: calculate())
+# btn1.grid(row=4, column=0, columnspan=3)
 
-lbl1 = Label(root, text="Matrix determinant = ")
-lbl1.grid(row=5, column=0, columnspan=3)
+# lbl1 = Label(root, text="Matrix determinant = ")
+# lbl1.grid(row=5, column=0, columnspan=3)
 
 root.mainloop()
