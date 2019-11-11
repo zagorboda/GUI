a8ffa469ba9191a7d21a7a3f3e2fb0a91b4b3911
zagorboda <zagorboda@gmail.com>
2019-11-12 00:58:07 +0300

    Add matrix sum

--- Matrix Calculator.py
+++ Matrix Calculator.py
@@ -15,12 +15,8 @@ btn_start_det = Button(root, text="Matrix determinant", command=lambda: Determin
 btn_start_det.grid(row=0, column=0)
 btn_start_rank = Button(root, text="Matrix rank", command=lambda: Rank(root))
 btn_start_rank.grid(row=0, column=1)
-
-# General widgets for several classes
-entry_m = Entry(root)
-entry_n = Entry(root)
-label_entry_m = Label(root, text="Enter m:")
-label_entry_n = Label(root, text="Enter n:")
+btn_start_sum = Button(root, text="Matrix sum", command=lambda: MatrixSum(root))
+btn_start_sum.grid(row=1, column=0)
 
 
 class Determinant:
@@ -28,20 +24,25 @@ class Determinant:
     def __init__(self, main):
         btn_start_det.grid_forget()
         btn_start_rank.grid_forget()
-        label_entry_m.grid(row=0, column=0)
-        label_entry_n.grid(row=1, column=0)
-        entry_m.grid(row=0, column=1)
-        entry_n.grid(row=1, column=1)
-        btn_start = Button(root, text="Start", command=lambda: self.check(btn_start))
-        btn_start.grid(row=3, column=0)
+        btn_start_sum.grid_forget()
+        self.entry_m = Entry(root)
+        self.entry_n = Entry(root)
+        self.label_entry_m = Label(root, text="Enter m:")
+        self.label_entry_n = Label(root, text="Enter n:")
+        self.label_entry_m.grid(row=0, column=0)
+        self.label_entry_n.grid(row=1, column=0)
+        self.entry_m.grid(row=0, column=1)
+        self.entry_n.grid(row=1, column=1)
+        self.btn_start = Button(root, text="Start", command=lambda: self.check())
+        self.btn_start.grid(row=3, column=0)
 
-    def check(self, btn_start):
+    def check(self):
         show_empty_input = False
         show_non_digit_input = False
         m_correct_input = 0
         n_correct_input = 0
         global m
-        m = entry_m.get()
+        m = self.entry_m.get()
         if (m == "") & (show_empty_input == False):
             showwarning("Warning", "You have empty input")
             show_empty_input = True
@@ -58,7 +59,7 @@ class Determinant:
                     show_non_digit_input = True
 
         global n
-        n = entry_n.get()
+        n = self.entry_n.get()
         if (n == "") & (show_empty_input == False):
             showwarning("Warning", "You have empty input")
             show_empty_input = True
@@ -80,11 +81,11 @@ class Determinant:
             pass
 
         if m_correct_input == 2 and n_correct_input == 2 and n == m:
-            entry_m.grid_forget()
-            entry_n.grid_forget()
-            btn_start.grid_forget()
-            label_entry_n.grid_forget()
-            label_entry_m.grid_forget()
+            self.entry_m.grid_forget()
+            self.entry_n.grid_forget()
+            self.btn_start.grid_forget()
+            self.label_entry_n.grid_forget()
+            self.label_entry_m.grid_forget()
             self.show()
 
     def show(self):
@@ -129,20 +130,25 @@ class Rank:
     def __init__(self, main):
         btn_start_det.grid_forget()
         btn_start_rank.grid_forget()
-        label_entry_m.grid(row=0, column=0)
-        label_entry_n.grid(row=1, column=0)
-        entry_m.grid(row=0, column=1)
-        entry_n.grid(row=1, column=1)
-        btn_start = Button(root, text="Start", command=lambda: self.check(btn_start))
-        btn_start.grid(row=3, column=0)
+        btn_start_sum.grid_forget()
+        self.entry_m = Entry(root)
+        self.entry_n = Entry(root)
+        self.label_entry_m = Label(root, text="Enter m:")
+        self.label_entry_n = Label(root, text="Enter n:")
+        self.label_entry_m.grid(row=0, column=0)
+        self.label_entry_n.grid(row=1, column=0)
+        self.entry_m.grid(row=0, column=1)
+        self.entry_n.grid(row=1, column=1)
+        self.btn_start = Button(root, text="Start", command=lambda: self.check())
+        self.btn_start.grid(row=3, column=0)
 
-    def check(self, btn_start):
+    def check(self):
         show_empty_input = False
         show_non_digit_input = False
         m_correct_input = 0
         n_correct_input = 0
         global m
-        m = entry_m.get()
+        m = self.entry_m.get()
         if (m == "") & (show_empty_input == False):
             showwarning("Warning", "You have empty input")
             show_empty_input = True
@@ -159,7 +165,7 @@ class Rank:
                     show_non_digit_input = True
 
         global n
-        n = entry_n.get()
+        n = self.entry_n.get()
         if (n == "") & (show_empty_input == False):
             showwarning("Warning", "You have empty input")
             show_empty_input = True
@@ -175,11 +181,11 @@ class Rank:
                     show_non_digit_input = True
 
         if m_correct_input == 2 and n_correct_input == 2 and n == m:
-            entry_m.grid_forget()
-            entry_n.grid_forget()
-            btn_start.grid_forget()
-            label_entry_n.grid_forget()
-            label_entry_m.grid_forget()
+            self.entry_m.grid_forget()
+            self.entry_n.grid_forget()
+            self.btn_start.grid_forget()
+            self.label_entry_n.grid_forget()
+            self.label_entry_m.grid_forget()
             self.show()
 
     def show(self):
@@ -211,12 +217,115 @@ class Rank:
                         if show_non_digit_input == False:
                             showwarning("Warning", "Your input include non digit symbols")
                             show_non_digit_input = True
-
-
         np.array(c)
         c = np.reshape(c, (m, n))
         lbl1 = Label(root, text="Matrix rank = %.4f" % np.linalg.matrix_rank(c))
         lbl1.grid(row=i + 2, column=0, columnspan=3)
+
+
+class MatrixSum:
+
+    def __init__(self, main):
+        btn_start_det.grid_forget()
+        btn_start_rank.grid_forget()
+        btn_start_sum.grid_forget()
+        self.entry_m = Entry(root)
+        self.entry_n = Entry(root)
+        self.label_entry_m = Label(root, text="Enter m:")
+        self.label_entry_n = Label(root, text="Enter n:")
+        self.label_entry_m.grid(row=0, column=0)
+        self.label_entry_n.grid(row=1, column=0)
+        self.entry_m.grid(row=0, column=1)
+        self.entry_n.grid(row=1, column=1)
+        self.btn_start = Button(root, text="Start", command=lambda: self.check())
+        self.btn_start.grid(row=3, column=0)
+
+    def check(self):
+        show_empty_input = False
+        show_non_digit_input = False
+        m_correct_input = 0
+        n_correct_input = 0
+        global m
+        m = self.entry_m.get()
+        if (m == "") & (show_empty_input == False):
+            showwarning("Warning", "You have empty input")
+            show_empty_input = True
+        else:
+            try:
+                m = int(m)
+                m_correct_input += 1
+                if m > 0:
+                    m_correct_input += 1
+
+            except ValueError:
+                if show_non_digit_input == False:
+                    showwarning("Warning", "Your input include non digit symbols")
+                    show_non_digit_input = True
+
+        global n
+        n = self.entry_n.get()
+        if (n == "") & (show_empty_input == False):
+            showwarning("Warning", "You have empty input")
+            show_empty_input = True
+        else:
+            try:
+                n = int(n)
+                n_correct_input += 1
+                if n > 0:
+                    n_correct_input += 1
+            except ValueError:
+                if show_non_digit_input == False:
+                    showwarning("Warning", "Your input include non digit symbols")
+                    show_non_digit_input = True
+
+        if m_correct_input == 2 and n_correct_input == 2 and n == m:
+            self.entry_m.grid_forget()
+            self.entry_n.grid_forget()
+            self.btn_start.grid_forget()
+            self.label_entry_n.grid_forget()
+            self.label_entry_m.grid_forget()
+            self.show()
+
+    def show(self):
+        a = [[0 for x in range(n)] for y in range(m)]
+        c = [[0 for x in range(n)] for y in range(m)]
+        for i in range(m):
+            for j in range(n):
+                entry1 = Entry(root, width=10)
+                entry1.grid(row=i, column=j)
+                label_empty = Label(root, width=5)
+                label_empty.grid(row=i, column=n+1)
+                entry2 = Entry(root, width=10)
+                entry2.grid(row=i, column=n+j+2)
+                a[i][j] = entry1
+                c[i][j] = entry2
+        btn1 = Button(root, text="Calculate", command=lambda: self.calculate(m, n, a, c))
+        btn1.grid(row=i + 1, column=0, columnspan=3)
+
+    def calculate(self, m, n, a, c):
+        show_empty_input = False
+        show_non_digit_input = False
+        b = [[0 for x in range(n)] for y in range(m)]
+        d = [[0 for x in range(n)] for y in range(m)]
+        for i in range(m):
+            for j in range(n):
+                b[i][j] = a[i][j].get()
+                d[i][j] = c[i][j].get()
+                if (b[i][j] == "") & (d[i][j] == "") & (show_empty_input == False):
+                    showwarning("Warning", "You have empty input")
+                    show_empty_input = True
+                else:
+                    try:
+                        b[i][j] = int(a[i][j].get())
+                        d[i][j] = int(c[i][j].get())
+                    except ValueError:
+                        if show_non_digit_input == False:
+                            showwarning("Warning", "Your input include non digit symbols")
+                            show_non_digit_input = True
+        for i in range(m):
+            for j in range(n):
+                label = Label(root, width=3, text=b[i][j] + d[i][j])
+                label.grid(row=i+m+1, column=j)
 
 
 root.mainloop()
