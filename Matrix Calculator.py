1b4bd95ade6b705f74e85a7283c079e7fe6fac2a
zagorboda <zagorboda@gmail.com>
2019-11-11 09:40:04 +0300

    Add new class: matrix rank . Refactor widget's code

--- Matrix Calculator.py
+++ Matrix Calculator.py
@@ -5,34 +5,37 @@ from tkinter.messagebox import *
 # TODO
 #  resize labels in x or y if number longer than that label
 #  firstly show empty warning , then non digit
-#  for determinant check if m == n
 #  add top menu that include links to main page and all functions
-#  input matrix shape: if empty_input warning shown dot show non digit warning
+#  input matrix shape: if empty_input warning shown don't show non digit warning
 
 root = Tk()
 root.title("Matrix Calculator")
 
 btn_start_det = Button(root, text="Matrix determinant", command=lambda: Determinant(root))
 btn_start_det.grid(row=0, column=0)
+btn_start_rank = Button(root, text="Matrix rank", command=lambda: Rank(root))
+btn_start_rank.grid(row=0, column=1)
+
+# General widgets for several classes
+entry_m = Entry(root)
+entry_n = Entry(root)
+label_entry_m = Label(root, text="Enter m:")
+label_entry_n = Label(root, text="Enter n:")
 
 
 class Determinant:
 
     def __init__(self, main):
         btn_start_det.grid_forget()
-        label_entry_m = Label(root, text="Enter m:")
+        btn_start_rank.grid_forget()
         label_entry_m.grid(row=0, column=0)
-        label_entry_n = Label(root, text="Enter n:")
         label_entry_n.grid(row=1, column=0)
-        entry_m = Entry(root)
         entry_m.grid(row=0, column=1)
-        entry_n = Entry(root)
         entry_n.grid(row=1, column=1)
-        btn_start = Button(root, text="Start", command=lambda: self.check(entry_m, entry_n, btn_start, label_entry_n,
-                                                                     label_entry_m))
+        btn_start = Button(root, text="Start", command=lambda: self.check(btn_start))
         btn_start.grid(row=3, column=0)
 
-    def check(self, entry_m, entry_n, btn_start, label_entry_n, label_entry_m):
+    def check(self, btn_start):
         show_empty_input = False
         show_non_digit_input = False
         m_correct_input = 0
@@ -109,7 +112,6 @@ class Determinant:
                     try:
                         b[i][j] = int(a[i][j].get())
                         c.append(b[i][j])
-                        print(b[i][j])
                     except ValueError:
                         if show_non_digit_input == False:
                             showwarning("Warning", "Your input include non digit symbols")
@@ -118,10 +120,103 @@ class Determinant:
         if len(c) == m*n:
             np.array(c)
             c = np.reshape(c, (m, n))
-            print(c)
-            print("%.4f" % np.linalg.det(c))
             lbl1 = Label(root, text="Matrix determinant = %.4f" % np.linalg.det(c))
             lbl1.grid(row=i + 2, column=0, columnspan=3)
+
+
+class Rank:
+
+    def __init__(self, main):
+        btn_start_det.grid_forget()
+        btn_start_rank.grid_forget()
+        label_entry_m.grid(row=0, column=0)
+        label_entry_n.grid(row=1, column=0)
+        entry_m.grid(row=0, column=1)
+        entry_n.grid(row=1, column=1)
+        btn_start = Button(root, text="Start", command=lambda: self.check(btn_start))
+        btn_start.grid(row=3, column=0)
+
+    def check(self, btn_start):
+        show_empty_input = False
+        show_non_digit_input = False
+        m_correct_input = 0
+        n_correct_input = 0
+        global m
+        m = entry_m.get()
+        if (m == "") & (show_empty_input == False):
+            showwarning("Warning", "You have empty input")
+            show_empty_input = True
+        else:
+            try:
+                m = int(m)
+                m_correct_input += 1
+                if m > 0:
+                    m_correct_input += 1
+
+            except ValueError:
+                if show_non_digit_input == False:
+                    showwarning("Warning", "Your input include non digit symbols")
+                    show_non_digit_input = True
+
+        global n
+        n = entry_n.get()
+        if (n == "") & (show_empty_input == False):
+            showwarning("Warning", "You have empty input")
+            show_empty_input = True
+        else:
+            try:
+                n = int(n)
+                n_correct_input += 1
+                if n > 0:
+                    n_correct_input += 1
+            except ValueError:
+                if show_non_digit_input == False:
+                    showwarning("Warning", "Your input include non digit symbols")
+                    show_non_digit_input = True
+
+        if m_correct_input == 2 and n_correct_input == 2 and n == m:
+            entry_m.grid_forget()
+            entry_n.grid_forget()
+            btn_start.grid_forget()
+            label_entry_n.grid_forget()
+            label_entry_m.grid_forget()
+            self.show()
+
+    def show(self):
+        a = [[0 for x in range(n)] for y in range(m)]
+        for i in range(m):
+            for j in range(n):
+                entry = Entry(root, width=10)
+                entry.grid(row=i, column=j)
+                a[i][j] = entry
+        btn1 = Button(root, text="Calculate", command=lambda: self.calculate(m, n, a))
+        btn1.grid(row=i + 1, column=0, columnspan=3)
+
+    def calculate(self, m, n, a):
+        c = []
+        show_empty_input = False
+        show_non_digit_input = False
+        b = [[0 for x in range(n)] for y in range(m)]
+        for i in range(m):
+            for j in range(n):
+                b[i][j] = a[i][j].get()
+                if (b[i][j] == "") & (show_empty_input == False):
+                    showwarning("Warning", "You have empty input")
+                    show_empty_input = True
+                else:
+                    try:
+                        b[i][j] = int(a[i][j].get())
+                        c.append(b[i][j])
+                    except ValueError:
+                        if show_non_digit_input == False:
+                            showwarning("Warning", "Your input include non digit symbols")
+                            show_non_digit_input = True
+
+
+        np.array(c)
+        c = np.reshape(c, (m, n))
+        lbl1 = Label(root, text="Matrix rank = %.4f" % np.linalg.matrix_rank(c))
+        lbl1.grid(row=i + 2, column=0, columnspan=3)
 
 
 root.mainloop()
