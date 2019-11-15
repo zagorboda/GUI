d8cc516ff52e999647cd09247f08e49be90cb620
zagorboda <zagorboda@gmail.com>
2019-11-15 17:37:03 +0300

    Fixed bug with stacked output labels. Added padding for all widgets

--- Matrix Calculator.py
+++ Matrix Calculator.py
@@ -12,14 +12,16 @@ from tkinter.messagebox import *
 root = Tk()
 root.title("Matrix Calculator")
 
-btn_start_det = Button(root, text="Matrix determinant", command=lambda: Determinant())
-btn_start_det.grid(row=0, column=0)
-btn_start_rank = Button(root, text="Matrix rank", command=lambda: Rank())
-btn_start_rank.grid(row=0, column=1)
-btn_start_sum = Button(root, text="Matrix sum", command=lambda: MatrixSum())
-btn_start_sum.grid(row=1, column=0)
-btn_start_sum = Button(root, text="Matrix multiplication", command=lambda: MatrixMultiplication())
-btn_start_sum.grid(row=1, column=1)
+# root.grid_columnconfigure(0, weight=1)
+
+btn_start_det = Button(root, width=15, text="Matrix determinant", command=lambda: Determinant())
+btn_start_det.grid(row=0, column=0, padx=5, pady=5)
+btn_start_rank = Button(root, width=15, text="Matrix rank", command=lambda: Rank())
+btn_start_rank.grid(row=0, column=1, padx=5, pady=5)
+btn_start_sum = Button(root, width=15, text="Matrix sum", command=lambda: MatrixSum())
+btn_start_sum.grid(row=1, column=0, padx=5, pady=5)
+btn_start_sum = Button(root, width=15, text="Matrix multiplication", command=lambda: MatrixMultiplication())
+btn_start_sum.grid(row=1, column=1, padx=5, pady=5)
 
 
 class Determinant:
@@ -38,12 +40,13 @@ class Determinant:
         self.entry_n = Entry(root)
         self.label_entry_m = Label(root, text="Enter m:")
         self.label_entry_n = Label(root, text="Enter n:")
-        self.label_entry_m.grid(row=0, column=0)
-        self.label_entry_n.grid(row=1, column=0)
+        self.label_entry_m.grid(row=0, column=0, padx=3, pady=3)
+        self.label_entry_n.grid(row=1, column=0, padx=3, pady=3)
         self.entry_m.grid(row=0, column=1)
         self.entry_n.grid(row=1, column=1)
         self.btn_start = Button(root, text="Start", command=lambda: self.check())
-        self.btn_start.grid(row=3, column=0)
+        self.btn_start.grid(row=3, column=0, padx=3, pady=3)
+        self.label = Label()
 
     def check(self):
         show_empty_input = False
@@ -106,10 +109,10 @@ class Determinant:
         for i in range(m):
             for j in range(n):
                 entry = Entry(root, width=10)
-                entry.grid(row=i, column=j)
+                entry.grid(row=i, column=j, padx=3, pady=3)
                 a[i][j] = entry
         btn1 = Button(root, text="Calculate", command=lambda: self.calculate(m, n, a))
-        btn1.grid(row=m + 1, column=0, columnspan=3)
+        btn1.grid(row=m + 1, column=0, columnspan=3, padx=3, pady=3)
 
     def calculate(self, m, n, a):
         c = []
@@ -134,8 +137,9 @@ class Determinant:
         if len(c) == m*n:
             np.array(c)
             c = np.reshape(c, (m, n))
-            lbl1 = Label(root, text="Matrix determinant = %.4f" % np.linalg.det(c))
-            lbl1.grid(row=m + 2, column=0, columnspan=3)
+            self.label.grid_forget()
+            self.label.configure(text="Matrix determinant = %f" % np.linalg.det(c))
+            self.label.grid(row=m + 2, column=0, columnspan=n, padx=3, pady=3)
 
 
 class Rank:
@@ -156,10 +160,11 @@ class Rank:
         self.label_entry_n = Label(root, text="Enter n:")
         self.label_entry_m.grid(row=0, column=0)
         self.label_entry_n.grid(row=1, column=0)
-        self.entry_m.grid(row=0, column=1)
-        self.entry_n.grid(row=1, column=1)
+        self.entry_m.grid(row=0, column=1, padx=3, pady=3)
+        self.entry_n.grid(row=1, column=1, padx=3, pady=3)
         self.btn_start = Button(root, text="Start", command=lambda: self.check())
-        self.btn_start.grid(row=3, column=0)
+        self.btn_start.grid(row=3, column=0, padx=3, pady=3)
+        self.label = Label()
 
     def check(self):
         show_empty_input = False
@@ -174,9 +179,8 @@ class Rank:
         else:
             try:
                 m = int(m)
-                m_correct_input += 1
                 if m > 0:
-                    m_correct_input += 1
+                    m_correct_input = 1
 
             except ValueError:
                 if show_non_digit_input == False:
@@ -191,15 +195,14 @@ class Rank:
         else:
             try:
                 n = int(n)
-                n_correct_input += 1
                 if n > 0:
-                    n_correct_input += 1
+                    n_correct_input = 1
             except ValueError:
                 if show_non_digit_input == False:
                     showwarning("Warning", "Your input include non digit symbols")
                     show_non_digit_input = True
 
-        if m_correct_input == 2 and n_correct_input == 2 and n == m:
+        if m_correct_input == 1 and n_correct_input == 1:
             _list = root.winfo_children()
 
             for item in _list:
@@ -216,10 +219,10 @@ class Rank:
         for i in range(m):
             for j in range(n):
                 entry = Entry(root, width=10)
-                entry.grid(row=i, column=j)
+                entry.grid(row=i, column=j, padx=3, pady=3)
                 a[i][j] = entry
         btn1 = Button(root, text="Calculate", command=lambda: self.calculate(m, n, a))
-        btn1.grid(row=m + 1, column=0, columnspan=3)
+        btn1.grid(row=m + 1, column=0, columnspan=3, padx=3, pady=3)
 
     def calculate(self, m, n, a):
         c = []
@@ -242,8 +245,9 @@ class Rank:
                             show_non_digit_input = True
         np.array(c)
         c = np.reshape(c, (m, n))
-        lbl1 = Label(root, text="Matrix rank = %.4f" % np.linalg.matrix_rank(c))
-        lbl1.grid(row=m + 2, column=0, columnspan=3)
+        self.label.grid_forget()
+        self.label.configure(text="Matrix rank = " + str(np.linalg.matrix_rank(c)))
+        self.label.grid(row=m + 2, column=0, columnspan=n, padx=3, pady=3)
 
 
 class MatrixSum:
@@ -261,12 +265,13 @@ class MatrixSum:
         self.entry_n = Entry(root)
         self.label_entry_m = Label(root, text="Enter m:")
         self.label_entry_n = Label(root, text="Enter n:")
-        self.label_entry_m.grid(row=0, column=0)
-        self.label_entry_n.grid(row=1, column=0)
-        self.entry_m.grid(row=0, column=1)
-        self.entry_n.grid(row=1, column=1)
+        self.label_entry_m.grid(row=0, column=0, padx=3, pady=3)
+        self.label_entry_n.grid(row=1, column=0, padx=3, pady=3)
+        self.entry_m.grid(row=0, column=1, padx=3, pady=3)
+        self.entry_n.grid(row=1, column=1, padx=3, pady=3)
         self.btn_start = Button(root, text="Start", command=lambda: self.check())
-        self.btn_start.grid(row=3, column=0)
+        self.btn_start.grid(row=3, column=0, padx=3, pady=3)
+        self.label = Label()
 
     def check(self):
         show_empty_input = False
@@ -324,15 +329,15 @@ class MatrixSum:
         for i in range(m):
             for j in range(n):
                 entry1 = Entry(root, width=10)
-                entry1.grid(row=i, column=j)
+                entry1.grid(row=i, column=j, padx=3, pady=3)
                 label_empty = Label(root, width=5)
-                label_empty.grid(row=i, column=n+1)
+                label_empty.grid(row=i, column=n+1, padx=3, pady=3)
                 entry2 = Entry(root, width=10)
-                entry2.grid(row=i, column=n+j+2)
+                entry2.grid(row=i, column=n+j+2, padx=3, pady=3)
                 a[i][j] = entry1
                 c[i][j] = entry2
         btn1 = Button(root, text="Calculate", command=lambda: self.calculate(m, n, a, c))
-        btn1.grid(row=m + 1, column=0, columnspan=3)
+        btn1.grid(row=m + 1, column=0, columnspan=3, padx=3, pady=3)
 
     def calculate(self, m, n, a, c):
         show_empty_input = False
@@ -354,10 +359,15 @@ class MatrixSum:
                         if show_non_digit_input == False:
                             showwarning("Warning", "Your input include non digit symbols")
                             show_non_digit_input = True
+
+        # for i in range(m):
+        #     for j in range(n):
+        #         self.label.grid_forget()
+
         for i in range(m):
             for j in range(n):
-                label = Label(root, width=3, text=b[i][j] + d[i][j])
-                label.grid(row=i+m+1, column=j)
+                self.label = Label(root, width=3, text=b[i][j] + d[i][j])
+                self.label.grid(row=i+m+2, column=j, padx=3, pady=3)
 
 
 class MatrixMultiplication:
@@ -379,20 +389,20 @@ class MatrixMultiplication:
 
         self.label_entry_m1 = Label(root, text="Enter m1:")
         self.label_entry_n1 = Label(root, text="Enter n1:")
-        self.label_entry_m1.grid(row=0, column=0)
-        self.label_entry_n1.grid(row=1, column=0)
-        self.entry_m1.grid(row=0, column=1)
-        self.entry_n1.grid(row=1, column=1)
+        self.label_entry_m1.grid(row=0, column=0, padx=3, pady=3)
+        self.label_entry_n1.grid(row=1, column=0, padx=3, pady=3)
+        self.entry_m1.grid(row=0, column=1, padx=3, pady=3)
+        self.entry_n1.grid(row=1, column=1, padx=3, pady=3)
 
         self.label_entry_m2 = Label(root, text="Enter m2:")
         self.label_entry_n2 = Label(root, text="Enter n2:")
-        self.label_entry_m2.grid(row=0, column=2)
-        self.label_entry_n2.grid(row=1, column=2)
-        self.entry_m2.grid(row=0, column=3)
-        self.entry_n2.grid(row=1, column=3)
+        self.label_entry_m2.grid(row=0, column=2, padx=3, pady=3)
+        self.label_entry_n2.grid(row=1, column=2, padx=3, pady=3)
+        self.entry_m2.grid(row=0, column=3, padx=3, pady=3)
+        self.entry_n2.grid(row=1, column=3, padx=3, pady=3)
 
         self.btn_start = Button(root, text="Start", command=lambda: self.check())
-        self.btn_start.grid(row=3, column=0)
+        self.btn_start.grid(row=3, column=0, padx=3, pady=3)
 
     def check(self):
         show_empty_input = False
@@ -482,9 +492,9 @@ class MatrixMultiplication:
         for i in range(m1):
             for j in range(n1):
                 entry1 = Entry(root, width=10)
-                entry1.grid(row=i+1, column=j)
+                entry1.grid(row=i+1, column=j, padx=3, pady=3)
                 label_empty = Label(root, width=5)
-                label_empty.grid(row=i+1, column=n1+1)
+                label_empty.grid(row=i+1, column=n1+1, padx=3, pady=3)
                 # entry2 = Entry(root, width=10)
                 # entry2.grid(row=i+1, column=n1+j+2)
                 a[i][j] = entry1
@@ -497,12 +507,12 @@ class MatrixMultiplication:
                 # label_empty = Label(root, width=5)
                 # label_empty.grid(row=i+1, column=n1+1)
                 entry2 = Entry(root, width=10)
-                entry2.grid(row=i+1, column=n1+j+2)
+                entry2.grid(row=i+1, column=n1+j+2, padx=3, pady=3)
                 # a[i][j] = entry1
                 c[i][j] = entry2
 
         btn1 = Button(root, text="Calculate", command=lambda: self.calculate(m1, n1, m2, n2, a, c))
-        btn1.grid(row=m1+1, column=0)
+        btn1.grid(row=m1+1, column=0, padx=3, pady=3)
 
     def calculate(self, m1, n1, m2, n2, a, c):
         show_empty_input = False
@@ -548,7 +558,7 @@ class MatrixMultiplication:
         for i in range(m1):
             for j in range(n2):
                 label = Label(root, width=3, text=e[i][j])
-                label.grid(row=m2+i+3, column=j)
+                label.grid(row=m2+i+3, column=j, padx=3, pady=3)
 
 
 root.mainloop()
